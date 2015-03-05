76cfc5bd19616284c61c46d70b6aa72180fec9ff
maperiaz <maperiaz@med.umich.edu>
2015-03-05 17:09:38 -0500

    fixed points

--- src/main/java/edu/umich/umms/worldofworkcraft/domain/Achievement.java
+++ src/main/java/edu/umich/umms/worldofworkcraft/domain/Achievement.java
@@ -1,8 +1,13 @@
 package edu.umich.umms.worldofworkcraft.domain;
 
 import lombok.Data;
+import org.neo4j.graphdb.Direction;
+import org.springframework.data.neo4j.annotation.Fetch;
 import org.springframework.data.neo4j.annotation.GraphId;
 import org.springframework.data.neo4j.annotation.NodeEntity;
+import org.springframework.data.neo4j.annotation.RelatedTo;
+
+import java.util.Set;
 
 @NodeEntity
 public class Achievement {
@@ -30,9 +35,19 @@ public class Achievement {
         this.point = point;
     }
 
+    public Learner getLearner() {
+        return learner;
+    }
+
+    public void setLearner(Learner learner) {
+        this.learner = learner;
+    }
+
+
     @GraphId
    private Long id;
    private String name;
    private Long point;
+    private Learner learner;
 
 }
