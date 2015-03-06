e23e057951ac713ebaa73c34a752627e8992f7d4
Leif Myklebust <lmyklebu@10.21.121.7>
2015-03-05 21:04:13 -0500

    add ability to create achievement

--- src/main/java/edu/umich/umms/worldofworkcraft/controller/AchievementController.java
+++ src/main/java/edu/umich/umms/worldofworkcraft/controller/AchievementController.java
@@ -1,16 +1,15 @@
 package edu.umich.umms.worldofworkcraft.controller;
 
-import edu.umich.umms.worldofworkcraft.domain.Achievement;
-import edu.umich.umms.worldofworkcraft.domain.Learner;
-import edu.umich.umms.worldofworkcraft.repository.AchievementRepository;
-import edu.umich.umms.worldofworkcraft.repository.LearnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.List;
+import edu.umich.umms.worldofworkcraft.domain.Achievement;
+import edu.umich.umms.worldofworkcraft.repository.AchievementRepository;
 
 @Controller
 @RequestMapping("/achievement")
@@ -18,6 +17,11 @@ import java.util.List;
 public class AchievementController {
     @Autowired
     AchievementRepository achievementRepository;
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Achievement> createAchievement(@RequestBody Achievement achievement) {
+        return new ResponseEntity<Achievement>(achievementRepository.save(achievement), HttpStatus.OK);
+    }
 
 
    // @RequestMapping(value = "/find/{achievement}", method = RequestMethod.GET)
