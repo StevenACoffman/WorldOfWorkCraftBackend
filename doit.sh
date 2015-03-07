22582e6db4a6c5186e263fe62c503b94ba3e269f
Steve Coffman <gears@umich.edu>
2015-03-06 21:36:48 -0500

    Fix readme

--- doit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mvn clean install
-
-echo ************************** COPYING FILES *********************
-cp target/worldofworkcraft-0.0.1-SNAPSHOT.war /usr/local/opt/tomcat/libexec/webapps/worldofworkcraft.war
